bcb0272d81433e4079681284c48534868405c423
Harika dikkala <92453060+haarikad@users.noreply.github.com>
2022-06-28 19:59:32 +0530

    Removed Wrappers

--- DockerDSL.groovy
+++ DockerDSL.groovy
@@ -9,7 +9,7 @@ job('Docker DSL') {
         scm('H/20 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        // nodejs('nodejs') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
